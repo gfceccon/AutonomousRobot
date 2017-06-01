48184b95c6c78f9920d77190590c9d88575a77b5
gabrielestrela <gabrielestrela92@gmail.com>
2017-05-31 21:00:07 -0300

    Map
    Created map

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	private Rigidbody rigid;
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();//pegando o tipo to componente
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigid.velocity = new Vector3 (1, 0, 0);
+	}
+}
